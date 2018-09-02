9c04daf8c0b8b23afc735860cc13e9d60a9dd65c
Thangmam <infimatet@gmail.com>
2018-09-02 07:59:40 +0530

    project init

---
+++ README.md
@@ -0,0 +1,6 @@
+# smooth_star_rating 0.0.1
+
+A Star rating with swipe enabled increment/decrement
+## Getting Started
+
+
---
+++ lib/smooth_star_rating.dart
@@ -0,0 +1,77 @@
+library smooth_star_rating;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+typedef void RatingChangeCallback(double rating);
+
+class StarRating extends StatelessWidget {
+  final int starCount;
+  final double rating;
+  final RatingChangeCallback onRatingChanged;
+  final Color color;
+  final Color borderColor;
+  final double size;
+  final bool allowHalfRating;
+
+  StarRating(
+      {this.starCount = 5,
+      this.rating = 0.0,
+      this.onRatingChanged,
+      this.color,
+      this.borderColor,
+      this.size,
+      this.allowHalfRating = true});
+
+  Widget buildStar(BuildContext context, int index) {
+    Icon icon;
+    if (index >= rating) {
+      icon = new Icon(
+        Icons.star_border,
+        color: borderColor ?? Theme.of(context).primaryColor,
+        size: size ?? 25.0,
+      );
+    } else if (index > rating - (allowHalfRating ? 0.5 : 1.0) &&
+        index < rating) {
+      icon = new Icon(
+        Icons.star_half,
+        color: color ?? Theme.of(context).primaryColor,
+        size: size ?? 25.0,
+      );
+    } else {
+      icon = new Icon(
+        Icons.star,
+        color: color ?? Theme.of(context).primaryColor,
+        size: size ?? 25.0,
+      );
+    }
+
+    return new GestureDetector(
+      onHorizontalDragUpdate: (dragDetails) {
+        RenderBox box = context.findRenderObject();
+        var _pos = box.globalToLocal(dragDetails.globalPosition);
+        var i = _pos.dx / size;
+        var newRating = allowHalfRating ? i : i.round().toDouble();
+        if (newRating > starCount) {
+          newRating = starCount.toDouble();
+        }
+        if (newRating < 0) {
+          newRating = 0.0;
+        }
+        this.onRatingChanged ?? onRatingChanged(newRating);
+      },
+      child: icon,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.transparent,
+      child: new Wrap(
+          alignment: WrapAlignment.start,
+          children: new List.generate(
+              starCount, (index) => buildStar(context, index))),
+    );
+  }
+}
