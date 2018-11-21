0e1da3f65e55047f290838ed90b23f669eb6a844
thangrobul infimate <infimatet@gmail.com>
2018-11-21 17:33:28 +0530

    added example

---
+++ example/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var rating = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rating bar demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+            child: SmoothStarRating(
+          rating: rating,
+          size: 45,
+          starCount: 5,
+          onRatingChanged: (value) {
+            setState(() {
+              rating = value;
+            });
+          },
+        )),
+      ),
+    );
+  }
+}
