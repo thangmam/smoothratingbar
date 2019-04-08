8bd3d9a0f7bd42097a3a8884c5050308822b4a1d
MatzeMarcel <marcel.garus@gmail.com>
2019-04-08 15:47:11 +0200

    Refactored code to be more idiomatic

--- lib/smooth_star_rating.dart
+++ lib/smooth_star_rating.dart
@@ -1,82 +1,74 @@
 library smooth_star_rating;
 
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-
-typedef void RatingChangeCallback(double rating);
 
 class SmoothStarRating extends StatelessWidget {
+  SmoothStarRating({
+    this.starCount = 5,
+    this.rating = 0.0,
+    this.onRatingChanged,
+    this.color,
+    this.borderColor,
+    this.size,
+    this.allowHalfRating = true,
+  })  : assert(starCount != null),
+        assert(rating != null),
+        assert(allowHalfRating != null);
+
   final int starCount;
   final double rating;
-  final RatingChangeCallback onRatingChanged;
+  final void Function(double rating) onRatingChanged;
   final Color color;
   final Color borderColor;
   final double size;
   final bool allowHalfRating;
 
-  SmoothStarRating(
-      {this.starCount = 5,
-      this.rating = 0.0,
-      this.onRatingChanged,
-      this.color,
-      this.borderColor,
-      this.size,
-      this.allowHalfRating = true}) {
-    assert(this.rating != null);
-  }
-
-  Widget buildStar(BuildContext context, int index) {
-    Icon icon;
-    if (index >= rating) {
-      icon = new Icon(
-        Icons.star_border,
-        color: borderColor ?? Theme.of(context).primaryColor,
-        size: size ?? 25.0,
-      );
-    } else if (index > rating - (allowHalfRating ? 0.5 : 1.0) &&
-        index < rating) {
-      icon = new Icon(
-        Icons.star_half,
-        color: color ?? Theme.of(context).primaryColor,
-        size: size ?? 25.0,
-      );
-    } else {
-      icon = new Icon(
-        Icons.star,
-        color: color ?? Theme.of(context).primaryColor,
-        size: size ?? 25.0,
-      );
-    }
-
-    return new GestureDetector(
-      onTap: () {
-        if (this.onRatingChanged != null) onRatingChanged(index + 1.0);
-      },
-      onHorizontalDragUpdate: (dragDetails) {
-        RenderBox box = context.findRenderObject();
-        var _pos = box.globalToLocal(dragDetails.globalPosition);
-        var i = _pos.dx / size;
-        var newRating = allowHalfRating ? i : i.round().toDouble();
-        if (newRating > starCount) {
-          newRating = starCount.toDouble();
-        }
-        if (newRating < 0) {
-          newRating = 0.0;
-        }
-        if (this.onRatingChanged != null) onRatingChanged(newRating);
-      },
-      child: icon,
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Wrap(
+        alignment: WrapAlignment.start,
+        children: List.generate(starCount, (i) => _buildStar(context, i)),
+      ),
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return new Material(
-      color: Colors.transparent,
-      child: new Wrap(
-          alignment: WrapAlignment.start,
-          children: new List.generate(
-              starCount, (index) => buildStar(context, index))),
+  Widget _buildStar(BuildContext context, int index) {
+    Icon icon;
+    if (index >= rating) {
+      icon = Icon(
+        Icons.star_border,
+        color: borderColor ?? Theme.of(context).primaryColor,
+        size: size ?? 25,
+      );
+    } else if (allowHalfRating && index > rating - 1) {
+      icon = Icon(
+        Icons.star_half,
+        color: color ?? Theme.of(context).primaryColor,
+        size: size ?? 25,
+      );
+    } else {
+      icon = Icon(
+        Icons.star,
+        color: color ?? Theme.of(context).primaryColor,
+        size: size ?? 25,
+      );
+    }
+
+    var ratingChangedCallback = onRatingChanged ?? (_) {};
+
+    return GestureDetector(
+      onTap: () => ratingChangedCallback(index + 1.0),
+      onHorizontalDragUpdate: (dragDetails) {
+        RenderBox box = context.findRenderObject();
+        var pos = box.globalToLocal(dragDetails.globalPosition);
+        var i = pos.dx / size;
+        var newRating =
+            (allowHalfRating ? i : i.roundToDouble()).clamp(0, starCount);
+        ratingChangedCallback(newRating);
+      },
+      child: icon,
     );
   }
 }
